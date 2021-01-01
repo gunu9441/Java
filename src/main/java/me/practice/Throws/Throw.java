3b329ab81d57fbb086e277336dcf60d9f74f30b7
Gunu <gunu9441@gmail.com>
2021-01-01 17:39:22 +0900

    change the name

---
+++ src/main/java/me/practice/Throws/Throw.java
@@ -0,0 +1,20 @@
+package me.practice.Throws;
+
+public class Throw {
+    public void sayNick(String nick){
+        try{
+            if("fool".equals(nick)){
+                throw new FoolException();
+            }
+            System.out.println("Your character is "+nick);
+        } catch( FoolException e){
+            System.err.println("FoolException occurs");
+        }
+    }
+
+    public static void main(String[] args) {
+        Throw thrw = new Throw();
+        thrw.sayNick("fool");
+        thrw.sayNick("genious");
+    }
+}
