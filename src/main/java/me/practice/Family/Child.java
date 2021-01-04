f5a7f6b56eeb532bbc4d933433091b9edea41ae0
Gunu <gunu9441@gmail.com>
2021-01-04 15:04:16 +0900

    test super method in inheritance

---
+++ src/main/java/me/practice/Family/Child.java
@@ -0,0 +1,29 @@
+package me.practice.Family;
+
+public class Child extends Parent {
+    int b;
+    Child(){
+        super(3);
+        System.out.println("Child");
+        b = 20;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hey");
+        Child chd = new Child();
+        System.out.println(chd.b);
+        System.out.println(chd.a);
+    }
+}
+
+class Parent{
+    int a;
+    Parent(){
+        a = 10;
+        System.out.println("Parent_1");
+    }
+    Parent(int n){
+        a=n;
+        System.out.println("Parent_2");
+    }
+}
