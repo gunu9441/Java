05ed5f696e4cbf9b7370b652c6887d2059d38e97
Gunu <gunu9441@gmail.com>
2021-01-08 13:41:11 +0900

    compare with python syntax

--- src/main/java/me/practice/Family/Child.java
+++ src/main/java/me/practice/Family/Child.java
@@ -1,26 +1,45 @@
 package me.practice.Family;
 
 public class Child extends Parent {
-    int b;
-    Child(){
-        super(3);
+    public String name;
+
+    Child(String name){
+        super();
         System.out.println("Child");
-        b = 20;
+        this.name = name;
+    }
+
+    public void show_name(){
+        System.out.println(this.name);
     }
 
     public static void main(String[] args) {
-        System.out.println("hey");
-        Child chd = new Child();
-        System.out.println(chd.b);
-        System.out.println(chd.a);
+        Parent p = new Child("Korea");
+        p.show();
+        System.out.println(p.population);
+        System.out.println(p.name);
     }
 }
 
 class Parent{
-    int a;
+
+    public String name;
+    public String population;
+    public String capital;
+
     Parent(){
-        a = 10;
         System.out.println("Parent");
+        this.name = "country";
+        this.capital = "capital";
+        this.population = "population";
     }
-    Parent(int n){a=n;}
+
+    public void show(){
+        System.out.println("parent");
+        System.out.println(this.name + this.capital + this.population);
+    }
+
+    public void show_print(){
+        System.out.println("print function");
+    }
 }
